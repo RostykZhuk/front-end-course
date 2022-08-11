23f4fe9c8fc2c91888c87bcce6a3f640afc3c944
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-08-11 11:56:02 +0300

    proj 2

---
+++ proj 2/script.js
@@ -0,0 +1,22 @@
+// variables
+
+let openBtn = document.getElementById('open-btn');
+let closeBtn = document.getElementById('close-btn');
+let modalContainer = document.getElementById('modal-container');
+
+// event listeners
+
+openBtn.addEventListener('click', function() {
+    modalContainer.style.display = 'block';
+
+});
+
+closeBtn.addEventListener('click', function() {
+    modalContainer.style.display = 'none';
+} );
+
+window.addEventListener('click', function(e) {
+    if (e.target == modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+} );
