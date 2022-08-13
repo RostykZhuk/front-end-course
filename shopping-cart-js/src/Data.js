69842b85bd07762d1b1aac31fdc7f998d6861ff7
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-08-13 12:56:55 +0300

    shopping cart js

---
+++ shopping-cart-js/src/Data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+    id:"sdfsdfsf",
+    name:"Casual Shirt",
+    price:45,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img:"images/img-1.jpg"
+},{
+    id:"sfd",
+    name:"Office Shirt",
+    price:100,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img:"images/img-2.jpg"
+},{
+    id:"gh",
+    name:"T Shirt",
+    price:25,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img:"images/img-3.jpg"
+},{
+    id:"ada",
+    name:"Mens Suit",
+    price:45,
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img:"images/img-4.jpg"
+}];
