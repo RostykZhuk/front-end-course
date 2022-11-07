09f11d6987c4131c162c72651f5fc1da25a6774a
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-07 21:19:16 +0200

    Promise chaining (then)

--- Asynchronous js/index.js
+++ Asynchronous js/index.js
@@ -5,7 +5,7 @@ let stocks = {
   toppings: ['chocolate', 'peanuts'],
 };
 
-let is_shop_open = true;
+let is_shop_open = false;
 
 let order = (time, work) => {
   return new Promise((resolve, reject) => {
@@ -18,4 +18,44 @@ let order = (time, work) => {
     }
   });
 };
-order(2000, () => console.log(`${stocks.Fruits[0]} was selected`));
+
+// step 1
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
+  // step 2
+  .then(() => {
+    return order(0000, () => console.log('production has started'));
+  })
+
+  // step 3
+  .then(() => {
+    return order(2000, () => console.log('Fruit has been chopped'));
+  })
+
+  // step 4
+  .then(() => {
+    return order(1000, () =>
+      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
+    );
+  })
+
+  // step 5
+  .then(() => {
+    return order(1000, () => console.log('start the machine'));
+  })
+
+  // step 6
+  .then(() => {
+    return order(2000, () =>
+      console.log(`ice cream placed on ${stocks.holder[1]}`)
+    );
+  })
+
+  // step 7
+  .then(() => {
+    return order(3000, () => console.log(`${stocks.toppings[0]} as toppings`));
+  })
+
+  // Step 8
+  .then(() => {
+    return order(2000, () => console.log('Serve Ice Cream'));
+  });
