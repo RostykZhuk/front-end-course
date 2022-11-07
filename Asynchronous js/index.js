b63e1b45c66e766a07587f39e881235c710c47e2
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-07 15:00:21 +0200

    practising callbacks and setTimeout

--- Asynchronous js/index.js
+++ Asynchronous js/index.js
@@ -1,19 +1,42 @@
-function one(call_two) {
-  console.log('step 1 complete. Please call step 2');
-  call_two();
-}
-function two() {
-  console.log('step 2');
-}
-one(two);
+let stocks = {
+  Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'peanuts'],
+};
 
-// -------------------
+let order = (fruit_name, call_production) => {
+  setTimeout(() => {
+    console.log(`${stocks.Fruits[fruit_name]} was selected`);
+    call_production();
+  }, 2000);
+};
 
-function three() {
-  console.log('step 3');
-  four();
-}
-function four() {
-  console.log('step 4');
-}
-three();
+let production = () => {
+  setTimeout(() => {
+    console.log('Production has started');
+
+    setTimeout(() => {
+      console.log('The fruit has been chopped');
+
+      setTimeout(() => {
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} were added`);
+
+        setTimeout(() => {
+          console.log('The machine was started');
+
+          setTimeout(() => {
+            console.log(`Ice cream placed on ${stocks.holder[0]}`);
+
+            setTimeout(() => {
+              console.log(`${stocks.toppings[0]} as toppings`);
+              console.log('Serve Ice cream');
+            }, 2000);
+          }, 3000);
+        }, 1000);
+      }, 1000);
+    }, 2000);
+  }, 0000);
+};
+
+order(0, production);
