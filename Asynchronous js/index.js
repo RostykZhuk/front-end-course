2f7a69fe249b4924dcaf868fc9865e6a7b362119
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-08 08:39:36 +0200

    try catch finnaly

--- Asynchronous js/index.js
+++ Asynchronous js/index.js
@@ -7,64 +7,15 @@ let stocks = {
 
 let is_shop_open = true;
 
-let order = (time, work) => {
-  return new Promise((resolve, reject) => {
-    if (is_shop_open) {
-      setTimeout(() => {
-        resolve(work());
-      }, 2000);
-    } else {
-      reject(console.log('Our shop is closed'));
-    }
-  });
-};
-
-// step 1
-order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
-  // step 2
-  .then(() => {
-    return order(0000, () => console.log('production has started'));
-  })
-
-  // step 3
-  .then(() => {
-    return order(2000, () => console.log('Fruit has been chopped'));
-  })
-
-  // step 4
-  .then(() => {
-    return order(1000, () =>
-      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
-    );
-  })
-
-  // step 5
-  .then(() => {
-    return order(1000, () => console.log('start the machine'));
-  })
-
-  // step 6
-  .then(() => {
-    return order(2000, () =>
-      console.log(`ice cream placed on ${stocks.holder[1]}`)
-    );
-  })
-
-  // step 7
-  .then(() => {
-    return order(3000, () => console.log(`${stocks.toppings[0]} as toppings`));
-  })
-
-  // Step 8
-  .then(() => {
-    return order(2000, () => console.log('Serve Ice Cream'));
-  })
-
-  // will work only when our variable is false
-  .catch(() => {
-    console.log('Customer left');
-  })
-
-  .finally(() => {
-    console.log('day ended shop is closed');
-  });
+async function order() {
+  try {
+    await a;
+  } catch (error) {
+    console.log('a doesnt exist');
+  } finally {
+    console.log('runs code anyways');
+  }
+}
+order().then(() => {
+  console.log('order placed');
+});
