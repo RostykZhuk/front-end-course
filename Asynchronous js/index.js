005e884199958e5d15168a5f1c18fd8a87761bcf
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-08 08:54:40 +0200

    practice await

--- Asynchronous js/index.js
+++ Asynchronous js/index.js
@@ -7,15 +7,25 @@ let stocks = {
 
 let is_shop_open = true;
 
-async function order() {
-  try {
-    await a;
-  } catch (error) {
-    console.log('a doesnt exist');
-  } finally {
-    console.log('runs code anyways');
-  }
+let topping_choice = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log('which topping would you like ?'));
+    }, 3000);
+  });
+};
+
+async function kitchen() {
+  console.log('A');
+  console.log('B');
+  console.log('C');
+  await topping_choice();
+  console.log('D');
+  console.log('E');
 }
-order().then(() => {
-  console.log('order placed');
-});
+
+kitchen();
+
+console.log('cleaning dishes');
+console.log('cleaning the tables');
+console.log('taking orders');
