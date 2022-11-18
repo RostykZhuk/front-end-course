5598a3b883cf90aa68f02d21f2b90d7e65a6ad44
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-18 09:11:22 +0200

    functions

--- Typescript couse/dist/main.js
+++ Typescript couse/dist/main.js
@@ -1,5 +1,9 @@
 // variables
-var hello = 'world';
+// let hello = 'world';
 // hello = 5 -- error bcs u can change only to the same type of data
 // defining a variable already with the type
-var a = 'Rostyk';
+// let a: string = 'Rostyk';
+var getFullName = function (name, surname) {
+    return name + ' ' + surname;
+};
+console.log(getFullName('Moster', 'Barack'));
--- Typescript couse/src/main.ts
+++ Typescript couse/src/main.ts
@@ -1,5 +1,10 @@
 // variables
-let hello = 'world';
+// let hello = 'world';
 // hello = 5 -- error bcs u can change only to the same type of data
 // defining a variable already with the type
-let a: string = 'Rostyk';
+// let a: string = 'Rostyk';
+
+const getFullName = (name: string, surname: string): string => {
+  return name + ' ' + surname;
+};
+console.log(getFullName('Moster', 'Barack'));
