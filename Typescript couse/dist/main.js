49f55bb0607e1be077245f92442547c61113c4de
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-18 09:02:46 +0200

    variables

---
+++ Typescript couse/dist/main.js
@@ -0,0 +1,5 @@
+// variables
+var hello = 'world';
+// hello = 5 -- error bcs u can change only to the same type of data
+// defining a variable already with the type
+var a = 'Rostyk';
---
+++ Typescript couse/src/main.ts
@@ -0,0 +1,5 @@
+// variables
+let hello = 'world';
+// hello = 5 -- error bcs u can change only to the same type of data
+// defining a variable already with the type
+let a: string = 'Rostyk';
