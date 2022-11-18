34cb3fffe76a45f6862afe04837db70add7674aa
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-18 11:13:33 +0200

    adding an eventlistener

--- Typescript couse/dist/Dom.js
+++ Typescript couse/dist/Dom.js
@@ -2,5 +2,10 @@
 // console.log('someElement', (someElement as any).value);
 // an incorecct fix
 // we havent an input at all and we can fix this like below
+// const someElement = document.querySelector('.foo') as HTMLInputElement;
+// console.log('someElement', someElement.value);
 var someElement = document.querySelector('.foo');
-console.log('someElement', someElement.value);
+someElement.addEventListener('blur', function (e) {
+    var target = e.target;
+    console.log('event', target.value);
+});
--- Typescript couse/src/Dom.ts
+++ Typescript couse/src/Dom.ts
@@ -3,5 +3,11 @@
 // console.log('someElement', (someElement as any).value);
 // an incorecct fix
 // we havent an input at all and we can fix this like below
-const someElement = document.querySelector('.foo') as HTMLInputElement;
-console.log('someElement', someElement.value);
+// const someElement = document.querySelector('.foo') as HTMLInputElement;
+// console.log('someElement', someElement.value);
+
+const someElement = document.querySelector('.foo');
+someElement.addEventListener('blur', (e) => {
+  const target = e.target as HTMLInputElement;
+  console.log('event', target.value);
+});
