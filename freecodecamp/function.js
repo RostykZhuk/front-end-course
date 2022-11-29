949240aea7bd68427e127a1d177021b70854f9ec
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-30 00:21:24 +0200

    asunc

--- freecodecamp/function.js
+++ freecodecamp/function.js
@@ -12,3 +12,12 @@
 // console.log(someVar);
 
 //Nesting a fuction
+function addSquare(a, b) {
+  function square(x) {
+    return x * x;
+  }
+  return square(a) + square(b);
+}
+let a = addSquare(2, 3);
+let b = addSquare(3, 4);
+let c = addSquare(4, 5);
