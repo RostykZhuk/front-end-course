95c32a7c516f478757b44dd156bed4072392d5ee
Rostyk Zhuk <rostukzhuk@gmail.com>
2022-11-30 23:22:53 +0200

    notification

---
+++ js-simple-proj/Notivication/script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+btn.addEventListener('click', () => {
+  createNotification();
+});
+function createNotification() {
+  const notif = document.createElement('div');
+  notif.classList.add('notification');
+  notif.innerText = 'This coding bootcamp us amazing';
+  container.appendChild(notif);
+  setTimeout(() => {
+    notif.remove();
+  }, 2000);
+}
